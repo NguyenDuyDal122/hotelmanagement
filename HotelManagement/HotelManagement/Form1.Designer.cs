ba94a74da160a8553c072e9df07bf5aa1957e255
DuyDal <2251010015dal@ou.edu.vn>
2025-03-22 21:13:59 +0700

    Thay doi

---
+++ HotelManagement/HotelManagement/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace HotelManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_username
+            // 
+            this.txt_username.Location = new System.Drawing.Point(290, 180);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(179, 22);
+            this.txt_username.TabIndex = 0;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(290, 261);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(179, 22);
+            this.txt_password.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(317, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "LOGIN";
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(333, 314);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(88, 62);
+            this.btn_login.TabIndex = 3;
+            this.btn_login.Text = "Đăng Nhập ";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(287, 242);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(287, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "User name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_username);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ HotelManagement/HotelManagement/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace HotelManagement
+{
+    public partial class Form1 : Form
+    {
+        string connectstring = @"Data Source=LAPTOP-CGUI40EU\MAY1;Initial Catalog=HotelManagement;Integrated Security=True;Encrypt=False";
+        SqlConnection connect;
+        SqlCommand cmd;
+        SqlDataAdapter adapter;
+        DataTable dt = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connect = new SqlConnection(connectstring);
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            string username = txt_username.Text.Trim();
+            string password = txt_password.Text.Trim();
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection connect = new SqlConnection(connectstring))
+                {
+                    connect.Open();
+                    string query = "SELECT role FROM [User] WHERE username = @username AND password_hash = @password";
+                    using (SqlCommand cmd = new SqlCommand(query, connect))
+                    {
+                        cmd.Parameters.AddWithValue("@username", username);
+                        cmd.Parameters.AddWithValue("@password", password);
+
+                        object result = cmd.ExecuteScalar();
+
+                        if (result != null)
+                        {
+                            string role = result.ToString();
+                            MessageBox.Show($"Đăng nhập thành công!\nQuyền: {role}", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                            Form mainForm = null;
+
+                            if (role == "admin")
+                            {
+                                mainForm = new HomeAdmin();
+                            }
+                            else if (role == "staff")
+                            {
+                                mainForm = new HomeStaff();
+                            }
+
+                            if (mainForm != null)
+                            {
+                                mainForm.Show();
+                                this.Hide();
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Sai tên đăng nhập hoặc mật khẩu!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi kết nối: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
